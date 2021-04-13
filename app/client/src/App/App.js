a0b14833ea035f98453e153e48ee07f9361d02b8
Tomolo997 <tomazovsenjak7@gmail.com>
2021-04-13 18:15:06 +0200

    first commmit 13 april

--- app/client/src/App/App.js
+++ app/client/src/App/App.js
@@ -1,51 +1,31 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Logo from '../../public/logo.svg'
+import Logo from '../../public/logo.svg';
 import './App.css';
 
 //GET request to server from client
-axios.get('http://localhost:8000/api/', {
-  
-})
-.then(response => {
-  
-})
-.catch(error => {
-
-});
+axios
+  .get('http://localhost:8000/api/', {})
+  .then((response) => {})
+  .catch((error) => {});
 
 //POST request to server from client
-axios.post('http://localhost:8000/api/', {
-  
-})
-.then(response => {
-  
-})
-.catch(error => {
-
-});
+axios
+  .post('http://localhost:8000/api/', {})
+  .then((response) => {})
+  .catch((error) => {});
 
 //PUT request to server from client
-axios.put('http://localhost:8000/api/', {
-  
-})
-.then(response => {
-  
-})
-.catch(error => {
-
-});
+axios
+  .put('http://localhost:8000/api/', {})
+  .then((response) => {})
+  .catch((error) => {});
 
 //DELETE request to server from client
-axios.delete('http://localhost:8000/api/', {
-  
-})
-.then(response => {
-  
-})
-.catch(error => {
-
-});
+axios
+  .delete('http://localhost:8000/api/', {})
+  .then((response) => {})
+  .catch((error) => {});
 
 class App extends Component {
   render() {
@@ -53,7 +33,9 @@ class App extends Component {
       <div>
         <header>
           <h1 className="main-heading">Here you come!</h1>
-          <h2 className="secondary-heading">Your MERN application is successfully generated.</h2>
+          <h2 className="secondary-heading">
+            Your MERN application is successfully generated.
+          </h2>
           <Logo />
         </header>
         <p className="paragraph">
