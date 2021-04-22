2a6a2bbeda2b36d35c3d3c967a2afbdbd525a3ae
Tomolo997 <tomazovsenjak7@gmail.com>
2021-04-22 20:50:01 +0200

    started front end landing page desing

--- app/client/src/App/App.js
+++ app/client/src/App/App.js
@@ -1,46 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from '../Components/NavBar/NavBar';
 import axios from 'axios';
 import Logo from '../../public/logo.svg';
 import './App.css';
 
-//GET request to server from client
-axios
-  .get('http://localhost:8000/api/', {})
-  .then((response) => {})
-  .catch((error) => {});
-
-//POST request to server from client
-axios
-  .post('http://localhost:8000/api/', {})
-  .then((response) => {})
-  .catch((error) => {});
-
-//PUT request to server from client
-axios
-  .put('http://localhost:8000/api/', {})
-  .then((response) => {})
-  .catch((error) => {});
-
-//DELETE request to server from client
-axios
-  .delete('http://localhost:8000/api/', {})
-  .then((response) => {})
-  .catch((error) => {});
-
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <h1 className="main-heading">Here you come!</h1>
-          <h2 className="secondary-heading">
-            Your MERN application is successfully generated.
-          </h2>
-          <Logo />
-        </header>
-        <p className="paragraph">
-          To get started, edit <code>src/App/App.js</code> and save to reload.
-        </p>
+      <div className="App">
+        <Navbar />
       </div>
     );
   }
---
+++ app/client/src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App/App.css';
+export default function NavBar() {
+  return (
+    <div className="NavBar">
+      <div>logo</div>
+      <div className="middle_navbar_div">
+        <div>About</div>
+        <div>pricing</div>
+        <div>Contact</div>
+      </div>
+      <div>Sing in</div>
+    </div>
+  );
+}
