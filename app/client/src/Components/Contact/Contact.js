ef673d768dc6c83bf4bfab96ef78979cb455b614
Tomaž Ovsenjak <tomazovsenjak@Tomazs-Air.fritz.box>
2021-04-23 20:25:12 +0200

    added layout of the landing page

--- app/client/src/Components/Contact/Contact.js
+++ app/client/src/Components/Contact/Contact.js
@@ -5,7 +5,6 @@ export default function Contact() {
     return (
         <div className="Contact">
             Contact
-            
         </div>
     )
 }
--- app/client/src/Components/NavBar/NavBar.js
+++ app/client/src/Components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import '../../App/App.css';
+import React from "react";
+import "../../App/App.css";
+import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div className="NavBar">
