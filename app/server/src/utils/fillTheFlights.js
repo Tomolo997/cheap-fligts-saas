1bd553d0a4ca40f4de02bc56d34583a7ea2ecc16
Tomaž Ovsenjak <tomazovsenjak@Tomazs-Air.fritz.box>
2021-04-23 20:39:19 +0200

    yea

--- app/server/src/utils/fillTheFlights.js
+++ app/server/src/utils/fillTheFlights.js
@@ -1,22 +1,22 @@
 //server.js
-require('dotenv').config();
-var cron = require('node-cron');
+require("dotenv").config();
+var cron = require("node-cron");
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const axios = require('axios');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const axios = require("axios");
 const port = process.env.PORT || 8000;
-require('dotenv').config();
-const User = require('../models/userModel');
-const Flights = require('../models/fligthsModel');
-const FlightResults = require('../models/flightsResult');
+require("dotenv").config();
+const User = require("../models/userModel");
+const Flights = require("../models/fligthsModel");
+const FlightResults = require("../models/flightsResult");
 
 //connection to the DB
 const DB = process.env.DATABASE;
 mongoose
   .connect(
-    'mongodb+srv://tomaz:tomaz@cheap-flights.bdzqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    "mongodb+srv://tomaz:tomaz@cheap-flights.bdzqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -25,35 +25,35 @@ mongoose
     }
   )
   .then(() => {
-    console.log('DB collection succesful');
+    console.log("DB collection succesful");
   });
 
 function TranfromDateToSuitableLink(date) {
-  const slice = date.slice(2, 10).split('-').join('');
+  const slice = date.slice(2, 10).split("-").join("");
   return slice;
 }
 
 const dateFormatForSkyscanner = (date) => {
   const months = [
-    '01',
-    '02',
-    '03',
-    '04',
-    '05',
-    '06',
-    '07',
-    '08',
-    '09',
-    '10',
-    '11',
-    '12',
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
   ];
   return date.getFullYear();
   // + '-' + months[date.getMonth()]
 };
 
 //get the current user
-exports.fillTheStates = async () => {
+const fillTheStates = async () => {
   try {
     //find the all of the flights, all flights je array
     const allFlights = await Flights.find();
@@ -173,12 +173,13 @@ exports.fillTheStates = async () => {
       const element = flightsForDB[i];
       await FlightResults.create(element);
     }
+
+    console.log("Succesfully filled the states");
+    process.exit(1);
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
 };
-cron.schedule('* * * * *', () => {
-  fillTheStates();
-  console.log('Filling the states');
-});
+fillTheStates();
+console.log("Filling the states");
