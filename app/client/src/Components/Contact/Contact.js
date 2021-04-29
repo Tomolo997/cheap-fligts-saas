95abc5fe9e3138ea08a514911b10f7e1a2878f4a
Tomolo997 <tomazovsenjak7@gmail.com>
2021-04-29 19:09:05 +0200

    contact list added

--- app/client/src/Components/Contact/Contact.js
+++ app/client/src/Components/Contact/Contact.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App/App.css';
 
 export default function Contact() {
+  const [contacted, setContacted] = useState(false);
+  const thankTheCustomer = (
+    <h1 className="contact_h1">
+      Thanks for contacting us! We will anwser your question shortly!
+    </h1>
+  );
+
+  const contactUs = (e) => {
+    e.preventDefault();
+    setContacted(true);
+  };
+
+  const contactPage = (
+    <form action="" className="contact_form">
+      <h1 className="contact_h1">Get in Touch with us</h1>
+      <label className="contact_label" htmlFor="name">
+        Full Name
+      </label>
+      <input
+        className="contact_input"
+        placeholder="John Doe"
+        type="text"
+        id="name"
+      />
+      <label className="contact_label" htmlFor="name">
+        E-mail
+      </label>
+      <input
+        className="contact_input"
+        placeholder="example@example.com"
+        type="text"
+        id="email"
+      />
+      <label className="contact_label" htmlFor="name">
+        Message
+      </label>
+      <textarea
+        placeholder="Arsenal is the best club in the world"
+        className="contact_textArea"
+        type="textarea"
+      />
+      <button onClick={contactUs} type="submit" className="contact_submit">
+        Contact us
+      </button>
+    </form>
+  );
+
   return (
-    <div className="Contact">
-      <form action="" className="contact_form">
-        Get in Touch with us
-        <label htmlFor="name">Name</label>
-        <input className="contact_input" type="text" id="name" />
-        <label htmlFor="name">Email</label>
-        <input className="contact_input" type="text" id="email" />
-        <label htmlFor="name">Message</label>
-        <textarea className="contact_textArea" type="textarea" />
-        <button type="submit" className="contact_submit">
-          Contact
-        </button>
-      </form>
-    </div>
+    <div className="Contact">{contacted ? thankTheCustomer : contactPage}</div>
   );
 }
