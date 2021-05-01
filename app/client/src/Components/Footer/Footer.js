2085bb55a6c1326aa4c67c86754d91107b8b3fa8
Tomaž Ovsenjak <tomazovsenjak@Tomazs-Air.fritz.box>
2021-05-01 15:39:12 +0200

    added footer basic design

--- app/client/src/Components/Footer/Footer.js
+++ app/client/src/Components/Footer/Footer.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import "../../App/App.css"
-
+import React from "react";
+import "../../App/App.css";
+import TwitterLogo from "../../../../Images/twitter_logo.svg";
 export default function Footer() {
-    return (
-        <div className="Footer">
-            Footer
-            
+  return (
+    <div className="Footer">
+      <div className="footer_div">
+        <div className="footer_socialMedia">
+          <div>
+            <img src={TwitterLogo}></img>
+          </div>
+          <div>Linked in</div>
         </div>
-    )
+        <div className="footer_info">
+          <div>info</div>
+        </div>
+        <div className="footer_support">
+          <div>Support</div>
+          <div>Terms of use</div>
+        </div>
+
+        <div className="footer_companyAt">@CostFriendlyFlights</div>
+      </div>
+    </div>
+  );
 }
--- app/client/src/Components/NavBar/NavBar.js
+++ app/client/src/Components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import '../../App/App.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../../App/App.css";
+import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div className="NavBar">
