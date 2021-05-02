12ff8a7d5a69737572589bbfb1823c7c510536cd
Tomaž Ovsenjak <tomazovsenjak@Tomazs-Air.fritz.box>
2021-05-02 14:15:01 +0200

    added the example page layout

--- app/client/src/Components/Example/Example.js
+++ app/client/src/Components/Example/Example.js
@@ -5,9 +5,22 @@ export default function Example() {
   return (
     <div className="Example">
       <h1 className="example_h1">See how it works</h1>
-      <div className="example_cube example_first_cube"></div>
-      <div className="example_cube example_second_cube"></div>
-      <div className="example_cube example_third_cube"></div>
+      <div className="example_cube example_first_cube">
+        <div>
+          <h1>Step 1</h1>
+        </div>
+      </div>
+      <div className="example_cube example_second_cube">
+        <div>
+          <h1>Step 2</h1>
+        </div>
+        <div></div>
+      </div>
+      <div className="example_cube example_third_cube">
+        <div>
+          <h1>Step 3</h1>
+        </div>
+      </div>
     </div>
   );
 }
