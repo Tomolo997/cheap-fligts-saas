ae0c4cd5851d190001dd9901a69597367e690b2b
Tomolo997 <tomazovsenjak7@gmail.com>
2021-05-19 17:55:48 +0200

    added the top cities

---
+++ app/server/src/utils/yea.js
@@ -0,0 +1,293 @@
+
+Katowice,
+Kaunas,
+Kazan
+Kharkiv,
+Khartoum,
+Kiev
+Kobe
+Kolkata
+Kosice,
+Kotor
+Krakow
+Krasnodar
+Kuala Lumpur
+Kumasi,
+Kuwait City
+Kyoto
+Lagos,
+Lahore
+Langkawi,
+Las Vegas
+Lausanne
+Leeds,
+Lhasa,
+Libreville,
+Lille
+Lima
+Linz
+Lisbon
+Liverpool
+Ljubljana
+Lodz
+London
+Los Angeles
+La Paz
+La Romana
+Luanda,
+Lublin
+Lubumbashi, 
+Lucerne
+Lucknow,
+Lusaka,
+Luxemburg
+Luxor
+Lyon
+Maastricht
+Macau
+Madrid
+Madurai
+Malaga
+Male,
+Malindi,
+Malmo,
+Managua,
+Manama,
+Manchester
+Mandalay,
+Manila
+Maputo Mozambique
+Mar Del Plata,
+Maracaibo
+Marrakech
+Marsa Alam,
+Marseilles
+Mashhad
+Mecca
+Medan,
+Medellin,
+Medina, 
+Melbourne
+Mexico City
+Miami
+Milan
+Minsk
+Mogadishu,
+Mombasa,
+Monaco
+Monastir,
+Montego Bay,
+Monterrey
+Montevideo
+Montpellier,
+Montreal
+Moscow
+Mostar
+Mumbai
+Munich
+Muscat
+Nadi,
+Nagoya,
+Naha,
+Nairobi
+Nancy
+Nanning
+Nantes
+Naples
+Nassau,
+Naypyidaw,
+Ndjamena
+New Orleans
+New Taipei City
+New York
+Newcastle Upon Tyne
+Newcastle,
+Nice
+Nicosia
+Nizhny Novgorod
+Nouakchott,
+Novi Sad,
+Novosibirsk,
+Nuremberg
+Nuuk,
+Odessa
+Oia Santorini
+Okayama
+Omsk
+Oran,
+Osaka
+Oslo
+Ostrava
+Ottawa
+Ouagadougou, Faso
+Oxford
+Padua,
+Palermo
+Palma De Mallorca
+Panama City
+Papeete, Polynesia
+Paphos
+Paris
+Parma
+Paro,
+Paros,
+Pattaya
+Perth
+Philadelphia
+Phnom Penh,
+Phoenix
+Phuket,
+Pisa
+Port Au Prince
+Port Elizabeth, 
+Port Harcourt,
+Port Louis,
+Port Moresby,
+Port Said
+Port Sudan,
+Port Vila,
+Portland
+Porto
+Porto Alegre
+Portofino,
+Positano
+Potsdam
+Prague
+Praia, Verde
+Pretoria
+Puebla
+Puerto Princesa,
+Puerto Vallarta,
+Punta Arenas,
+Punta Cana
+Punta Del Este,
+Putrajaya,
+Pyongyang
+Quebec City
+Quezon City,
+Quito
+Rabat,
+Recife,
+Regensburg,
+Reims
+Reykjavik,
+Riga
+Rijeka,
+Rio De Janeiro
+Riyadh
+Rome
+Rosario,
+Rostov-On-Don,
+Rotterdam
+Rouen
+Rovaniemi
+Saint Petersburg
+Salalah,
+Salamanca
+Salvador Da Bahia,
+Salzburg
+Samarkand,
+San Diego
+San Fransisco
+San Gimignano
+San Jose, Rica
+San Juan, Rico
+San Marino
+Sana,
+Santa Cruz De Tenerife
+Santiago De Compostela
+Santiago De Cuba
+Santiago,
+Santo Domingo, 
+Sao Paulo,
+Sapporo,
+Sarajevo
+Seattle
+Sendai
+Seoul
+Sevastopol,
+Seville
+Sfax,
+Shanghai
+Sharjah
+Sharm El-Sheikh
+Shenyang
+Shenzhen
+Shiraz,
+Siem Reap
+Siena
+Singapore
+Sintra
+Sofia
+Split
+St Denis Reunion
+St Louis
+Stockholm
+Strasbourg
+Surat
+Suva,
+Sydney
+Tabriz,
+Taichung,
+Taipei
+Tallinn
+Tampere
+Tangier,
+Tarragona
+Tashkent,
+Tbilisi
+Tehran,
+Tel Aviv
+Thessaloniki
+Thimphu,
+Tianjin
+Timisoara
+Tirana
+Tokyo
+Toledo
+Toronto
+Toulouse,
+Tours
+Tripoli,
+Tromsø
+Trondheim
+Tula
+Tunis
+Turin
+Ulaanbaatar,
+Ushuaia,
+Vaduz
+Valencia
+Valletta,
+Valparaiso,
+Vancouver
+Vatican City
+Venice
+Veracruz
+Verona
+Versailles
+Victoria,
+Vienna
+Vientiane,
+Vilnius
+Vladimir,
+Vladivostok,
+Volgograd,
+Warsaw
+Washington DC
+Wellington
+Windhoek,
+Wroclaw
+Wuhan
+Xiamen
+Xian,
+Yangon,
+Yaoundé,
+Yerevan
+Yogyakarta,
+Yokohama
+York
+Zagreb
+Zanzibar
+Zaragoza,
+Zurich
