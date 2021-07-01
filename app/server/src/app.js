2e5a7810121ee3753bae282d53db138e5d5f95e2
Tomolo997 <tomazovsenjak7@gmail.com>
2021-07-01 10:57:00 +0200

    added

--- app/server/src/app.js
+++ app/server/src/app.js
@@ -26,14 +26,15 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cors({ origin: ['http://localhost:5000'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:4000'], credentials: true }));
 app.use(helmet());
 //middleware
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Express server is up and running.');
 });
 
 //mounting the routes
+app.use('/', viewsRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/flights', flightRouter);
 app.use('/api/v1/payment', paymentRouter);
